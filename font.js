473bf253295abfe131ae2f55cf9cb1d85690a65e
Noah <gamerpenguin2005@gmail.com>
2022-02-15 21:02:15 -0800

    redid all icons to be pixel perfect on a new grid, added icons
    added -
    add selection (line & fill)
    subtract selection (line & fill)
    intersect selection (line& fill)
    replace selection (line & fill)
    changed some class names

--- font.js
+++ font.js
@@ -1,41 +1,20 @@
 const svgtofont = require("svgtofont");
 const path = require("path");
-const { scale } = require('scale-that-svg')
-
-const fs = require('fs');
-const directoryPath = path.join(__dirname, 'icons');
-fs.readdir(directoryPath, function (err, files) {
-  //listing all files using forEach
-  files.forEach(function (file) {
-    // Do whatever you want to do with the file
-    fs.readFile("./icons/" + file, 'utf8', (err, data) => {
-      scale(data, { scale: 100 }).then((scaled) => {
-        fs.writeFile("./icons/" + file, scaled, err => {
-          if (err) {
-            console.error(err)
-            return
-          }
-          //file written successfully
-        })
-      })
-    })
-  })
-  setTimeout(() => {
-    svgtofont({
-      src: path.resolve(process.cwd(), "icons"), // svg path
-      dist: path.resolve(process.cwd(), "fonts"), // output path
-      fontName: "Hypericons", // font name
-      classNamePrefix: "hi",
-      css: true, // Create CSS files.
-      startUnicode: 0xea01, // unicode start number
-      // website = null, no demo html files
-      website: {
-        title: "Hypericons",
-        logo: path.resolve(process.cwd(), "svg", "git.svg"),
-        version: "0",
-      }
-    }).then(() => {
-      console.log('done!');
-    });
-  }, 100);
-})
+setTimeout(() => {
+  svgtofont({
+    src: path.resolve(process.cwd(), "icons"), // svg path
+    dist: path.resolve(process.cwd(), "fonts"), // output path
+    fontName: "Hypericons", // font name
+    classNamePrefix: "hi",
+    css: true, // Create CSS files.
+    startUnicode: 0xea01, // unicode start number
+    // website = null, no demo html files
+    website: {
+      title: "Hypericons",
+      logo: path.resolve(process.cwd(), "svg", "git.svg"),
+      version: "0",
+    }
+  }).then(() => {
+    console.log('done!');
+  });
+}, 100);
